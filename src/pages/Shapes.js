17536da9e30357c559685b5d5762f78b20ef5928
Foeppe Geyml <foeppegeyml@Foeppes-MacBook-Pro.local>
2018-09-16 16:19:29 -0700

    Add content

--- src/pages/Shapes.js
+++ src/pages/Shapes.js
@@ -38,6 +38,15 @@ class Shapes extends Component {
     return (
       <Grid>
         <CenteredRow>
+          <h1>Graphs!</h1>
+          <h4>
+            Credit:{" "}
+            <a href="http://rajapradhan.com/blogs/d3-js-v4-essentials/shapes/">
+              Raja Pradhan
+            </a>{" "}
+            who's code and explanations I modified here
+          </h4>
+          <h2>Line Graph</h2>
           <LineGraph
             dataSets={dataSets}
             width={500}
@@ -56,6 +65,29 @@ class Shapes extends Component {
               {isAreaShown ? "Hide Area" : "Show Area"}
             </Button>
           </ButtonToolbar>
+          <h3>Explanation:</h3>
+          <ul>
+            <li>
+              We have 2 data arrays containing the (x,y) points which when
+              joined will create the full line.
+            </li>
+            <li>
+              We use d3.line() function which returns a line generator and we
+              specified that the x coordinate will be calculated using the
+              xScale mapping and y coordinate will be calculated using the
+              yScale mapping.
+            </li>
+            <li>
+              We create a path element in the svg container and set the value of
+              the d attribute using the newly created line generator function y
+              passing the complete data set to the generator.
+            </li>
+            <li>
+              Using an area generator is similar to using a line generator. The
+              only difference is we specify the lower(x0,y0) and upper(x1,y1)
+              bounds on both x and y axes in the area generator
+            </li>
+          </ul>
         </CenteredRow>
       </Grid>
     );
